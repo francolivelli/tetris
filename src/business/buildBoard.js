31d70959d7b221ffd7388e557034a77403522e44
francolivelli <franco.livelli@gmail.com>
2023-09-11 00:09:12 -0300

    input
    player rotation and movement

--- src/business/buildBoard.js
+++ src/business/buildBoard.js
@@ -1,4 +1,5 @@
 import { defaultCell } from "./defaultCell";
+import { movePlayer } from "./playerController";
 import { transferToBoard } from "./tetrominoes";
 
 export const buildBoard = ({ rows, columns }) => {
@@ -12,6 +13,25 @@ export const buildBoard = ({ rows, columns }) => {
   };
 };
 
+const findDropPosition = ({ board, position, shape }) => {
+  let max = board.size.rows - position.row + 1;
+  let row = 0;
+
+  for (let i = 0; i < max; i++) {
+    const delta = { row: i, column: 0 };
+    const result = movePlayer({ delta, position, shape, board });
+    const { collided } = result;
+
+    if (collided) {
+      break;
+    }
+
+    row = position.row + i;
+  }
+
+  return { ...position, row };
+};
+
 export const nextBoard = ({ board, player, resetPlayer, addLinesCleared }) => {
   const { tetromino, position } = player;
 
@@ -19,13 +39,95 @@ export const nextBoard = ({ board, player, resetPlayer, addLinesCleared }) => {
     row.map((cell) => (cell.occupied ? cell : { ...defaultCell }))
   );
 
-  rows = transferToBoard({
-    className: tetromino.className,
-    isOccupied: player.collided,
+  const dropPosition = findDropPosition({
+    board,
     position,
-    rows,
-    shape: tetromino.shape,
+    shape: tetromino.shape
   });
 
-  return { rows, size: { ...board.size } };
+  const className = `${tetromino.className} ${
+    player.isFastDropping ? "" : "ghost"
+  }`;
+  rows = transferToBoard({
+    className,
+    isOccupied: player.isFastDropping,
+    position: dropPosition,
+    rows,
+    shape: tetromino.shape
+  });
+
+  if (!player.isFastDropping) {
+    rows = transferToBoard({
+      className: tetromino.className,
+      isOccupied: player.collided,
+      position,
+      rows,
+      shape: tetromino.shape
+    });
+  }
+
+  const blankRow = rows[0].map((_) => ({ ...defaultCell }));
+  let linesCleared = 0;
+  rows = rows.reduce((acc, row) => {
+    if (row.every((column) => column.occupied)) {
+      linesCleared++;
+      acc.unshift([...blankRow]);
+    } else {
+      acc.push(row);
+    }
+
+    return acc;
+  }, []);
+
+  if (linesCleared > 0) {
+    addLinesCleared(linesCleared);
+  }
+
+  if (player.collided || player.isFastDropping) {
+    resetPlayer();
+  }
+
+  return {
+    rows,
+    size: { ...board.size }
+  };
+};
+
+export const hasCollision = ({ board, position, shape }) => {
+  for (let y = 0; y < shape.length; y++) {
+    const row = y + position.row;
+
+    for (let x = 0; x < shape[y].length; x++) {
+      if (shape[y][x]) {
+        const column = x + position.column;
+
+        if (
+          board.rows[row] &&
+          board.rows[row][column] &&
+          board.rows[row][column].occupied
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+};
+
+export const isWithinBoard = ({ board, position, shape }) => {
+  for (let y = 0; y < shape.length; y++) {
+    const row = y + position.row;
+
+    for (let x = 0; x < shape[y].length; x++) {
+      if (shape[y][x]) {
+        const column = x + position.column;
+        const isValidPosition = board.rows[row] && board.rows[row][column];
+
+        if (!isValidPosition) return false;
+      }
+    }
+  }
+
+  return true;
 };
--- src/components/GameController/GameController.js
+++ src/components/GameController/GameController.js
@@ -1,5 +1,6 @@
 import "./GameController.css";
 import { Action, actionForKey } from "../../business/input";
+import { playerController } from "../../business/playerController";
 
 const GameController = ({
   board,
@@ -8,13 +9,26 @@ const GameController = ({
   setGameOver,
   setPlayer,
 }) => {
-  const onKeyDown = ({ code }) => {};
+  const onKeyDown = ({ code }) => {
+    const action = actionForKey(code);
+    handleInput({ action });
+  };
 
   const onKeyUp = ({ code }) => {
     const action = actionForKey(code);
     if (action === Action.Quit) {
       setGameOver(true);
     }
+  };
+
+  const handleInput = ({ action }) => {
+    playerController({
+      action,
+      board,
+      player,
+      setPlayer,
+      setGameOver,
+    });
   };
 
   return (
